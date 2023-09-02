a7b45173f772f715c3bf9b967380f5d59f4b4607
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-02 23:47:51 +0900

    프로젝트 생성, 테스트

---
+++ notice-board/src/main/java/NoticeBoard/noticeboard/controller/BoardController.java
@@ -0,0 +1,15 @@
+package NoticeBoard.noticeboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/**")
+public class BoardController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "/boards/hello";
+    }
+}
