7adc2c194f85f7dba6c7bdb0cbbdb0bc6535f4b7
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-03 23:38:45 +0900

    Mybatis설정

--- notice-board/src/main/java/NoticeBoard/noticeboard/NoticeBoardApplication.java
+++ notice-board/src/main/java/NoticeBoard/noticeboard/NoticeBoardApplication.java
@@ -1,9 +1,11 @@
 package NoticeBoard.noticeboard;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "NoticeBoard.noticeboard")
 public class NoticeBoardApplication {
 
 	public static void main(String[] args) {
