2cf2a4ea7072683bff714007754fcfaf63e9f40e
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-04 00:25:46 +0900

    Board 생성

---
+++ notice-board/src/main/java/NoticeBoard/noticeboard/domain/Board.java
@@ -0,0 +1,24 @@
+package NoticeBoard.noticeboard.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+
+    private Long boardId;
+    private String title;
+    private String content;
+    private String name;
+    private LocalDateTime createDate;
+    private int read;
+    private Long memberId;
+}
