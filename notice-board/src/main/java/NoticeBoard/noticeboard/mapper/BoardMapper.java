a36ea7e68c835a95097aa6a994ed94ef8942f6e2
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-04 00:42:52 +0900

    BoardMapper.xml

---
+++ notice-board/src/main/java/NoticeBoard/noticeboard/mapper/BoardMapper.java
@@ -0,0 +1,12 @@
+package NoticeBoard.noticeboard.mapper;
+
+import NoticeBoard.noticeboard.domain.Board;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardMapper {
+    int boardCount(); //곧 생성할 BoardMapper.xml 첫번째 sql문의 id와 같음
+    List<Board> getList();
+}
