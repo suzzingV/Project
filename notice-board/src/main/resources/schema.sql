0eaeb0bbf7a301b7e97c998b849e9ed0c818fc2a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-05 23:20:50 +0900

    더미 데이터 삽입

--- notice-board/src/main/resources/schema.sql
+++ notice-board/src/main/resources/schema.sql
@@ -5,4 +5,13 @@ CREATE TABLE tbl_board(
                           name varchar (30) not null,
                           primary key(boardId)
 );
-INSERT INTO tbl_board( title, content, name ) VALUES ('제목', '내용', '이름')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목1', '내용1', '이름1')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목2', '내용2', '이름2')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목3', '내용3', '이름3')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목4', '내용4', '이름4')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목5', '내용5', '이름5')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목6', '내용6', '이름6')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목7', '내용7', '이름7')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목8', '내용8', '이름8')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목9', '내용9', '이름9')
+INSERT INTO tbl_board( title, content, name ) VALUES ('제목10', '내용10', '이름10')
