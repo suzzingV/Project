439c420d9a7a935c2187fd39e73f0320a5478dab
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-09 23:55:21 +0900

    interface 추가

--- notice-board/src/main/java/NoticeBoard/noticeboard/mapper/BoardMapper.java
+++ notice-board/src/main/java/NoticeBoard/noticeboard/mapper/BoardMapper.java
@@ -10,4 +10,6 @@ public interface BoardMapper {
     int boardCount(); //곧 생성할 BoardMapper.xml 첫번째 sql문의 id와 같음
     List<Board> getList();
     Board getBoard(Long boardId);
+
+    void uploadBoard(Board board);
 }
