7b447c2468d3f6ffe3beb1dacc05bfdfb13f5342
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-15 23:40:43 +0900

    BoardService

--- notice-board/src/main/java/NoticeBoard/noticeboard/service/BoardService.java
+++ notice-board/src/main/java/NoticeBoard/noticeboard/service/BoardService.java
@@ -26,7 +26,18 @@ public class BoardService {
 
     public Board getBoard(Long boardId) { return boardMapper.getBoard(boardId);}
 
+    @Transactional
     public void uploadBoard(Board board) {
         boardMapper.uploadBoard(board);
+    }
+
+    @Transactional
+    public void updateBoard(Board board) {
+        boardMapper.updateBoard(board);
+    }
+
+    @Transactional
+    public void deleteBoard(Long boardId) {
+        boardMapper.deleteBoard(boardId);
     }
 }
